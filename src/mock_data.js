5180137c263f8de3d1a598865795f89cdbb8fc2b
Chad Quilling <chadquilling@gmail.com>
2021-11-06 11:30:13 -0600

    Add mockdata

---
+++ src/mock_data.js
@@ -0,0 +1,161 @@
+let mock = {
+  names = [
+    {
+      id: 1,
+      name: "Chad",
+      gender: "male",
+    },
+    {
+      id: 2,
+      name: "Ezra",
+      gender: "male",
+    },
+    {
+      id: 3,
+      name: "John",
+      gender: "male",
+    },
+    {
+      id: 4,
+      name: "Hannah",
+      gender: "female",
+    },
+    {
+      id: 5,
+      name: "Bethany",
+      gender: "female",
+    },
+    {
+      id: 6,
+      name: "Whitney",
+      gender: "female",
+    }
+  ],
+  objects = [
+    {
+      id: 1,
+      name: "tea cup",
+      size: "small",
+    },
+    {
+      id: 2,
+      name: "mushrooms",
+      size: "small",
+    },
+    {
+      id: 3,
+      name: "golf ball",
+      size: "small",
+    },
+    {
+      id: 4,
+      name: "teddy bear",
+      size: "large",
+    },
+    {
+      id: 5,
+      name: "bear",
+      size: "large",
+    },
+    {
+      id: 6,
+      name: "human",
+      size: "large",
+    },
+  ],
+  adjectives = [
+    {
+      id: 1,
+      name: "red",
+      type: "color",
+    },
+    {
+      id: 2,
+      name: "blue",
+      type: "color",
+    },
+    {
+      id: 3,
+      name: "green",
+      type: "color",
+    },
+    {
+      id: 4,
+      name: "lumpy",
+      size: "other",
+    },
+    {
+      id: 5,
+      name: "excited",
+      size: "large",
+    },
+    {
+      id: 6,
+      name: "phat",
+      size: "large",
+    },
+  ],
+  verbs = [
+    {
+      id: 1,
+      past: "ran",
+      tense: "past",
+    },
+    {
+      id: 2,
+      past: "stopped",
+      tense: "past",
+    },
+    {
+      id: 3,
+      past: "jumped",
+      tense: "past",
+    },
+    {
+      id: 4,
+      past: "eating",
+      tense: "present",
+    },
+    {
+      id: 5,
+      past: "nibble",
+      tense: "present",
+    },
+    {
+      id: 6,
+      past: "SWAG",
+      tense: "present",
+    },
+    {
+      id: 7,
+      past: "will jump",
+      tense: "future",
+    },
+    {
+      id: 8,
+      past: "will sleep",
+      tense: "future",
+    },
+    {
+      id: 9,
+      past: "will caress",
+      tense: "future",
+    },
+  ],
+  abverbs = [
+    {
+      id: 1,
+      past: "quickly",
+    },
+    {
+      id: 2,
+      past: "abnormally",
+    },
+    {
+      id: 3,
+      past: "bravely",
+    },
+  ]
+}
+
+export default mock;
